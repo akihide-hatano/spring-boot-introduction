0d0d9fe18bfbd72b27f7bb0dea6b872f1cc51bbd
akihide-hatano <a.h.19841202@gmail.com>
2026-05-04 17:04:56 +0900

    TaskServiceにて,タスクのリストを取得するためのメソッドを追加しました。

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -1,38 +1,30 @@
 package com.example.todo.controller.task;
 
-//リストを作成するためのクラス
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.todo.service.task.TaskService;
+
 @Controller
 public class TaskController {
 
     private static final Logger log = LoggerFactory.getLogger(TaskController.class);
 
+    //TaskServiceクラスのインスタンスを作成
+    private final TaskService taskService = new TaskService();
+
     //ハンドラー名
     @GetMapping("/tasks")
     public String list(Model model){
         log.info("list()が呼び出されました");
-        //modelにSpringBootを学ぼうと入力
-        var task1 = new TaskDTO(1,
-            "SpringBootを学ぼう",
-            "todoアプリを作ろう",
-            "todo");
 
-        var task2 = new TaskDTO(2L,
-             "Spring Securityについて学ぶ",
-             "ログイン機能を作ってみる", "todo");
 
-        var taskList = List.of(task1, task2);
-
-        model.addAttribute("taskList", taskList);
-        log.debug("model attribute taskList='{}' を設定しました", taskList);
-        log.debug("taskLists/list.html を表示します", taskList);
+        model.addAttribute("taskList", taskService.find());
+        log.debug("model attribute taskList='{}' を設定しました", taskService.find());
+        log.debug("taskLists/list.html を表示します", taskService.find());
         log.info("list()の処理が終了しました");
         return "tasks/list";
     }
--- src/main/java/com/example/todo/service/task/TaskService.java
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -1,39 +1,22 @@
 package com.example.todo.service.task;
 
-import com.example.todo.repository.task.TaskRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
+import com.example.todo.controller.task.TaskDTO;
+
 public class TaskService {
 
-    private final TaskRepository taskRepository;
+    public List<TaskDTO> find(){
+        var task1 = new TaskDTO(1,
+            "SpringBootを学ぼう",
+            "todoアプリを作ろう",
+            "todo");
 
-    public List<TaskEntity> find(TaskSearchEntity searchEntity) {
-        return taskRepository.select(searchEntity);
-    }
+        var task2 = new TaskDTO(2L,
+            "Spring Securityについて学ぶ",
+            "ログイン機能を作ってみる", "todo");
 
-    public Optional<TaskEntity> findById(long taskId) {
-        return taskRepository.selectById(taskId);
-    }
-
-    @Transactional
-    public void create(TaskEntity newEntity) {
-        taskRepository.insert(newEntity);
-    }
-
-    @Transactional
-    public void update(TaskEntity entity) {
-        taskRepository.update(entity);
-    }
-
-    @Transactional
-    public void delete(long id) {
-        taskRepository.delete(id);
-    }
+            //returrnにてリストを返すためにList.of()を使用して、task1とtask2をリストにまとめて返す
+            return List.of(task1, task2);
+        }
 }
